101c94d14c6ad9f385e602e0d838a47af07bcc32
Yonube <blazer2008.mail@gmail.com>
2024-12-10 16:42:05 -0600

    New can ids for drive train

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -51,16 +51,16 @@ public final class Constants {
     public static final double kBackLeftChassisAngularOffset = Math.PI;
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
-    // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 8;
-    public static final int kRearLeftDrivingCanId = 2;
-    public static final int kFrontRightDrivingCanId = 9;
-    public static final int kRearRightDrivingCanId = 6;
+   // SPARK MAX CAN IDs
+   public static final int kFrontLeftDrivingCanId = 1;
+   public static final int kRearLeftDrivingCanId = 3;
+   public static final int kFrontRightDrivingCanId = 5;
+   public static final int kRearRightDrivingCanId = 7;
 
-    public static final int kFrontLeftTurningCanId = 7;
-    public static final int kRearLeftTurningCanId = 1;
-    public static final int kFrontRightTurningCanId = 10;
-    public static final int kRearRightTurningCanId = 5;
+   public static final int kFrontLeftTurningCanId = 2;
+   public static final int kRearLeftTurningCanId = 4;
+   public static final int kFrontRightTurningCanId = 6;
+   public static final int kRearRightTurningCanId = 8;
 
     public static final boolean kGyroReversed = false;
   }
@@ -71,9 +71,6 @@ public final class Constants {
 
   public static final class AmpConstants{
     public static final int AmpMotor = 3;
-  }
-  public static final class ElevatorConstants{
-    public static final int ElevatorMotor = 11;
   }
 
   public static final class ModuleConstants {
